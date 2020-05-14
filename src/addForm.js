3c4fca48820ac08abf5cddd7fe27835a185404d6
Jess <jessicatanhc@gmail.com>
2020-05-14 14:11:43 +0800

    Add validation

--- src/addForm.js
+++ src/addForm.js
@@ -1,60 +1,75 @@
-import React, { useState } from "react";
+import React, { useState , useRef } from "react";
 
 function AddForm(props) {
     const [values, setValues] = useState({title:"", note:"", favorite:false });
+    const inputRef = useRef([]);
+    inputRef.current = [];
+
+   const addToRefs = el => {
+     if (el && !inputRef.current.includes(el)) {
+       inputRef.current.push(el);
+     }
+    };
     return  <div className="AddForm">
                 <div className="overlay">
                     <div className="popup">
                         <a className="close" onClick={()=>{props.close();}}>&times;</a>
                         <div className="content">
-                            <div className="form__group_popup">
-                                <input
-                                    id="title"
-                                    name="title"
-                                    className="form__field"
-                                    value={values.title}
-                                    placeholder="Title"
-                                    onChange={(e)=>{setValues({...values, "title":e.target.value})}}
-                                />
+                            <form>
+                                <div className="form__group_popup">
+                                    <input
+                                        id="title"
+                                        name="title"
+                                        className="form__field"
+                                        value={values.title}
+                                        placeholder="Title"
+                                        onChange={(e)=>{setValues({...values, "title":e.target.value})}}
+                                        ref={addToRefs}
+                                        required="required"
+                                    />
+                                    <label
+                                        htmlFor="title"
+                                        className="form__label"
+                                        >
+                                        Title
+                                    </label>
+                                </div>
+                                <div className="form__group_popup">
+                                    <textarea
+                                        ref={addToRefs}
+                                        name="note"
+                                        id="note"
+                                        rows="3"
+                                        className="form__field_custom"
+                                        value={values.note}
+                                        placeholder="Write your notes here!"
+                                        onChange={(e)=>{
+                                            setValues({...values, note:e.target.value})}}
+                                        required="required"
+                                    />
                                 <label
-                                    htmlFor="title"
-                                    className="form__label"
-                                    >
-                                    Title
+                                    htmlFor="note"
+                                    className="form__label2"
+                                >
+                                    Note
                                 </label>
                             </div>
-                            <div className="form__group_popup">
-                                <textarea
-                                    name="note"
-                                    id="note"
-                                    rows="3"
-                                    className="form__field_custom"
-                                    value={values.note}
-                                    placeholder="Write your notes here!"
-                                    onChange={(e)=>{
-                                        setValues({...values, note:e.target.value})}}
-                                />
-                            <label
-                                htmlFor="note"
-                                className="form__label2"
-                            >
-                                Note
-                            </label>
+                            <div>
+                            <button className="btnAdd"
+                                    type="submit"
+                                    onClick={()=>{
+                                    if(values.title && values.note){
+                                        setValues({title:"",note:""});
+                                        props.setNotes( values);
+                                        props.close();
+                                    }else{
+                                        !values.title ? inputRef.current[0].focus(): inputRef.current[1].focus();
+                                    }}
+                                    }>
+                                Add
+                            </button>
                         </div>
-                        <div>
-                        <button className="btnAdd"
-                                onClick={()=>{
-                                if(values.title && values.note){
-                                    setValues({title:"",note:""});
-                                    props.setNotes( values);
-                                    props.close();
-                                }else{
-                                    console.log("error message");
-                                }}
-                                }>
-                            Add
-                        </button>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
