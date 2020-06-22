8fc1aedb79cd7766df3a5c388ebdd2af317afca9
Jess <jessicatanhc@gmail.com>
2020-06-22 10:52:56 +0800

    Add user entity

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,6 +1,6 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { Note } from '../note/note.entity';
-
+import { User } from '../user/user.entity';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
  type: 'postgres',
@@ -9,6 +9,6 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
  username: 'jessica',
  password: null,
  database: 'notekeeper',
- entities: [Note],
+ entities: [ Note,User ],
  synchronize: true
 }
