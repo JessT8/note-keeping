2e9ad1e920118d108e00e4aa1198d2958f7065bd
Jess <jessicatanhc@gmail.com>
2020-08-07 17:31:00 +0800

    Get notes

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,54 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import AddForm from "./addForm";
 import DisplayNotes from "./displayNotes"
 import './styles.scss';
+import { useQuery, gql } from '@apollo/client';
+
+const NOTES = gql`
+      query{
+        notes{
+          id,
+          title,
+          description,
+          pin
+      }
+  }`;
+
 
 function App() {
   const [toggle, setToggle] = useState(false);
   const [notes, setNotes] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+  const { loading, error, data } = useQuery(NOTES);
+  useEffect(()=>{
+    if(error)
+       setErrorMessage('Error :(')
+    else if(data)
+       setNotes(data.notes);
+    },[data,error])
+
   const favorite=(id)=>{
-    const i = parseInt(id);
-    const n = [...notes];
-    n[i].favorite = !n[i].favorite;
+    const n = notes.map(note=> {
+        if (note.id === id) {
+            const pin = (note.pin=='true')? 'false': 'true'
+            note = {...note, pin};
+        }
+        return note;
+    });
     setNotes(n);
   }
+  const addNotes =()=>{
 
-  const updateNote = (values, i)=>{
-    const n = [...notes];
-    n[i] = values;
+  }
+  const updateNote = (values)=>{
+     const n = notes.map(note=> {
+        if (note.id === values.id) {
+            return values;
+        }
+        return note;
+    });
+     console.log(values);
     setNotes(n);
   }
   const deleteNote = (i)=>{
@@ -24,14 +56,16 @@ function App() {
     n.splice(i,1);
     setNotes(n);
   }
+
   return (
     <div className="App">
         <header>
             <h1>Note keeper</h1>
             <button className="toggleAdd" onClick={()=>{setToggle(true)}}>+</button>
         </header>
-      {toggle && <AddForm close={()=>{setToggle(false)}} setNotes={(value)=>{setNotes([...notes, value]);}}/>}
-      <DisplayNotes notes={notes} favorite={(e)=>{favorite(e)}} updateNote={(values, i)=>{updateNote(values, i)}} deleteNote={(i)=>{deleteNote(i)}}/>
+      {toggle && <AddForm close={()=>{setToggle(false)}} setNotes={(values)=>{addNotes(values);}}/>}
+      {errorMessage}
+      <DisplayNotes notes={notes} favorite={(e)=>{favorite(e)}} updateNote={(values)=>{ updateNote(values)}} deleteNote={(i)=>{deleteNote(i)}}/>
     </div>
   );
 }
