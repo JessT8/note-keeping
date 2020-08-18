14996943ef3d4f301619cf71ceac5aefc34903a0
Jess <jessicatanhc@gmail.com>
2020-08-18 09:40:06 +0800

    Add validation for landing page navigation

--- client/src/App.js
+++ client/src/App.js
@@ -7,30 +7,32 @@ import {
   Route,
   Link
 } from "react-router-dom";
-import Home from './home'
-import SignIn from './components/user/signin'
-import SignUp from './components/user/signup'
+import Home from './home';
+import LandingPage from './landingPage';
+import SignIn from './components/user/signin';
+import SignUp from './components/user/signup';
 
 function App() {
     const [user, setUser] = useState({token:"", username:""})
   return (
     <Router>
         <nav className="navbar navbar-expand-lg bg-dark navbar-dark" id="navBar">
-            <Link className="navbar-brand"  to="/">Notekeeper</Link>
+            <Link className="navbar-brand cinzel-font pl-5"  to="/">Notekeeper</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navContent" aria-controls="navContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navContent">
                 <ul className="navbar-nav ml-auto">
-                    <li className="nav-item mr-2" >
-                        <Link className="navLink text-white" to="/">Home</Link>
+                    <li className="nav-item" >
+                        <Link className="nunito-font navLink text-white" to="/">Home</Link>
                     </li>
-
-                    <li className="nav-item mr-2">
+                    <li className="text-white ml-3 mr-3"> |
+                    </li>
+                    <li className="nav-item">
                       {!user.token ?
-                      <Link className="navLink text-white"
-                            to="/signin">Signin</Link> :
-                      <a className="navLink text-white"
+                      <Link className="nunito-font navLink text-white"
+                            to="/signin">Sign in</Link> :
+                      <a className="navLink nunito-font text-white"
                          onClick={()=> {
                                 localStorage.clear();
                                 setUser({token:"", username:""});
@@ -38,14 +40,15 @@ function App() {
                             }}>
                             Sign Out</a>
                         }
-                    </li>)
+                    </li>
 
                 </ul>
             </div>
         </nav>
             <Switch>
                 <Route exact path="/">
-                    <Home />
+                    { user.token ? <Home />  : <LandingPage/> }
+
                 </Route>
                 <Route path="/signin">
                     <SignIn setUser = {(values)=>{setUser(values)}}/>
