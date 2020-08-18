5899175bd85767d150e68f6b7196fddb66f42cb4
Jess <jessicatanhc@gmail.com>
2020-08-18 09:39:43 +0800

    Add landing page

---
+++ client/src/landingPage.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import './styles.scss';
+
+function LandingPage() {
+  return (
+    <div className="container-fluid position-fixed h-100 w-100 bg-light">
+        <div className="row justify-content-center h-75 fade-in">
+            <div className="my-auto">
+                <p className="big-font cinzel-font">Write your notes!</p>
+                <h4 className="text-center">A quick and easy way for you to access all your notes</h4>
+                <div className="text-right pt-5 started-font"><a  href="/signin">Get Started &rarr;</a></div>
+            </div>
+
+        </div>
+        <div className="row justify-content-center fade-in">
+
+        </div>
+    </div>
+  );
+}
+
+export default LandingPage;
