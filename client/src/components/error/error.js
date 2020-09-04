9e5ef870ea14d7f8a23dfdd543231da0bb1bcb2e
Jess <jessicatanhc@gmail.com>
2020-09-05 00:38:34 +0800

    Add error page

--- client/src/components/error/error.js
+++ client/src/components/error/error.js
@@ -1,11 +1,26 @@
 import React from 'react';
-
+import './error.scss';
 function Error(){
     return (
-         <div className="container-fluid position-fixed h-100 w-100 bg-light">
-            <div className="row justify-content-center h-75">
+        <div className="container-fluid position-fixed h-100 w-100 bg-light">
+            <div className="justify-content-center">
                 <div className="my-auto">
-                    <p className="big-font nunito-font">Opps...Something went wrong :(</p>
+                    <div className="row pt-5">
+                        <div className='col'>
+                            <div className="face mx-auto">
+                                <div className="eye"></div>
+                                <div className="eye right"></div>
+                                <div className="mouth sad"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row  h-75 pt-5">
+                        <div className='col  justify-content-center'>
+                            <p className="error-font text-center nunito-font">
+                            Opps...Something went wrong...
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
