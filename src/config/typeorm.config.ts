487f315835182aa41a4cb9f5d4a9bd5c1f970397
Jess <jessicatanhc@gmail.com>
2021-02-11 23:55:43 +0800

    Modify configuration

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -5,15 +5,24 @@ import { Tag } from '../tag/tag.entity';
 import { NoteTag } from '../note-tag/note-tag.entity';
 import { join } from 'path';
 require('dotenv').config();
-const { DB_PORT, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_HOST } = process.env;
-
-export const typeOrmConfig: TypeOrmModuleOptions = {
- type:'postgres',
- host: DB_HOST,
- port: parseInt(DB_PORT),
- username: DB_USERNAME,
- password: DB_PASSWORD,
- database: DB_DATABASE,
- entities: [ User, Note, Tag, NoteTag ],
- synchronize: true
+const { DB_PORT, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_HOST, DATABASE_URL } = process.env;
+export let typeOrmConfig: TypeOrmModuleOptions = {
+  type:'postgres',
+  entities: [ User, Note, Tag, NoteTag ],
+  synchronize: true
+};
+if(DATABASE_URL){
+  typeOrmConfig = {
+    ...typeOrmConfig,
+    url:DATABASE_URL
+  }
+}else{
+typeOrmConfig = {
+    ...typeOrmConfig,
+    host: DB_HOST,
+    port: parseInt(DB_PORT),
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+  }
 }
